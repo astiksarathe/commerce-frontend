fdb9bf35531f4099e13a23048f331775be9e68ff
astiksarathe <astiksarathe.bce@gmail.com>
2024-06-19 10:06:36 +0530

    Policy Related Component

---
+++ src/pages/returnRefundPolicy/ReturnRefundPolicy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReturnRefundPolicy = () => {
+  return (
+    <div>ReturnRefundPolicy</div>
+  )
+}
+
+export default ReturnRefundPolicy
---
+++ src/pages/shippingDeliveryTerms/ShippingDeliveryTerms.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShippingDeliveryTerms = () => {
+  return (
+    <div>ShippingDeliveryTerms</div>
+  )
+}
+
+export default ShippingDeliveryTerms
