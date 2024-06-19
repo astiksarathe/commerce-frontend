bebb479909577b121f384875a9ca9d037e18dbb2
astiksarathe <astiksarathe.bce@gmail.com>
2024-06-19 17:04:33 +0530

    cart logic

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,6 +14,34 @@ const cartSlice = createSlice({
     cartDrawerHandler: (state, action) => {
       state.isCartDrawerOpen = action.payload || false;
     },
+    setCart: (state, action) => {
+      const cartList = JSON.parse(localStorage.getItem("cart"));
+      state.cartList = cartList;
+    },
+    addToCart: (state, action) => {
+      const { productId, quantity, title } = action.payload.productId;
+      let alreadyExistInCart = false;
+
+      const updatedCartList = state.map((product) => {
+        if (product.productId === productId) {
+          alreadyExistInCart = true;
+          return { productId, quantity, title };
+        }
+        return product;
+      });
+
+      if (!alreadyExistInCart) updatedCartList.push({ productId, quantity, title });
+
+      localStorage.setItem("cart", JSON.stringify(updatedCartList));
+
+      state.cartList = updatedCartList;
+    },
+    removeFromCart: (state, action) => {
+      const { productId } = action.payload.productId;
+      const updatedCartList = state.filter((product) => product.productId !== productId);
+      state.cartList = updatedCartList;
+      localStorage.setItem("cart", JSON.stringify(updatedCartList));
+    },
     // Additional reducers can be defined here if needed
   },
 });
