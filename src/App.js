d30a8751e2bde7ffe0eb1c5d2b595719afad35e5
astiksarathe <astiksarathe.bce@gmail.com>
2024-07-18 23:18:40 +0530

    Sync cart with localStorage

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import { Outlet } from "react-router-dom";
 import { CartDrawer } from "./components/cart";
 import QuickView from "./components/quickView/";
 import Checkout from "./components/checkout";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setCart } from "./features/cart";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setCart());
+  }, []);
   return (
     <>
       <Header />
