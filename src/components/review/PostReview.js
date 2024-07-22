7e8249db995043e7f1e2c21af0d1971a7f5e4810
astiksarathe <astiksarathe.bce@gmail.com>
2024-07-22 17:19:08 +0530

    Screen for review and PostReview

---
+++ src/components/review/PostReview.js
@@ -0,0 +1,127 @@
+import { MinusOutlined } from "@ant-design/icons";
+import { Button, Drawer, Form, Input, Rate, Space } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import React, { useEffect, useState } from "react";
+import "./review.scss";
+
+const PostReview = () => {
+  const [form] = Form.useForm();
+  const [rating, setRating] = useState(1);
+
+  const onFinish = (e) => {
+    console.log(e);
+  };
+  const onFinishFailed = () => {};
+  useEffect(() => {
+    return () => {
+      form.resetFields();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const ratingChangeHandler = (val) => {
+    if (val <= 0) setRating(1);
+    else setRating(val);
+  };
+  return (
+    <Drawer
+      className="login_drawer"
+      title="Review"
+      placement={"right"}
+      width={340}
+      onClose={() => {}}
+      open={true}
+      extra={
+        <Space>
+          <Button type="text" style={{ color: "white", fontSize: 14 }} onClick={() => {}}>
+            CLOSE
+            <MinusOutlined style={{ color: "white", fontSize: 14 }} />
+          </Button>
+        </Space>
+      }
+    >
+      <div className="post-review-container">
+        <div className="post-review-header">
+          <h1 className="post-review-title">Add a Review</h1>
+          <p className="review-instructions">
+            <span>Your email address will not be published. </span>
+            <span>
+              Required fields are marked <span className="required-mark">*</span>
+            </span>
+          </p>
+        </div>
+        <div className="select-rating">
+          <Rate value={rating} style={{ fontSize: "30px" }} onChange={ratingChangeHandler} />
+        </div>
+        <Form
+          form={form}
+          name="review"
+          initialValues={{
+            remember: true,
+          }}
+          layout="vertical"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          className="review-form"
+        >
+          <Form.Item
+            label="Your Review"
+            name="review"
+            rules={[
+              {
+                required: true,
+                message: "Please input your review!",
+              },
+            ]}
+          >
+            <TextArea
+              placeholder="Your Review"
+              autoSize={{ minRows: 5, maxRows: 5 }}
+              className="review-textarea"
+            />
+          </Form.Item>
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your name!",
+              },
+            ]}
+          >
+            <Input size="large" className="name-input" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+              {
+                type: "email",
+                message: "Invalid email address",
+              },
+            ]}
+          >
+            <Input size="large" className="email-input" />
+          </Form.Item>
+          <Button
+            type="primary"
+            block
+            className="submit-button"
+            htmlType="submit"
+            onClick={onFinish}
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </Drawer>
+  );
+};
+
+export default PostReview;
--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -1,7 +1,114 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Button, Progress, Rate } from "antd";
 import React from "react";
+import { green } from "@ant-design/colors";
+import PropTypes from "prop-types";
+import "./review.scss";
+import PostReview from "./PostReview";
+const ReviewItems = () => {
+  return (
+    <div className="review-item">
+      <div className="review-author">
+        <div className="author-info">
+          <div className="author-avatar">
+            <Avatar icon={<UserOutlined />} size={50} />
+          </div>
+          <div className="author-details">
+            <div className="author-stars">
+              <Rate disabled value={5} style={{ fontSize: "16px" }} />
+            </div>
+            <p className="author-rating hide">5 out of 5 stars</p>
+            <div className="author-meta">
+              <strong>Emily Selman</strong> <em className="author-verified">(verified owner)</em>
+              <span>-</span>
+              <time dateTime="">June 21, 2024</time>
+            </div>
+          </div>
+        </div>
 
-const Review = () => {
-  return <div>Review</div>;
+        <div className="review-text">
+          <p>
+            This is the bag of my dreams. I took it on my last vacation and was able to fit an
+            absurd amount of snacks for the many long and hungry flights.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
+const ReviewDataItem = ({ rating, percentage }) => {
+  return (
+    <div className="review-data-item">
+      <dt className="review-data-label">
+        <div className="review-data-star">
+          <Rate disabled value={rating} style={{ fontSize: "16px" }} />
+        </div>
+        <div aria-hidden="true" className="review-data-bar">
+          <Progress
+            percent={percentage}
+            percentPosition={{ align: "center", type: "inner" }}
+            size={["100%", 15]}
+            strokeLinecap="square"
+            strokeColor={green[6]}
+          />
+        </div>
+      </dt>
+      <dd className="review-data-percentage">{percentage}% </dd>
+    </div>
+  );
+};
+
+const Review = () => {
+  return (
+    <>
+      <div className="review-component">
+        <div className="review-header">
+          <h2 className="review-title">Reviews (5)</h2>
+          <div className="review-summary">
+            <div>
+              <div className="review-stars">
+                <Rate disabled value={5} />
+              </div>
+              <p className="review-rating hide">4 out of 5 stars</p>
+            </div>
+            <p className="review-count">Based on 1624 reviews</p>
+          </div>
+        </div>
+        <div className="review-data">
+          <dl className="review-data-list">
+            <ReviewDataItem rating={5} percentage={40} />
+            <ReviewDataItem rating={5} percentage={40} />
+            <ReviewDataItem rating={5} percentage={40} />
+            <ReviewDataItem rating={5} percentage={40} />
+            <ReviewDataItem rating={5} percentage={40} />
+          </dl>
+        </div>
+        <div className="review-share">
+          <Button size="large" block className="share-link">
+            Write a review
+          </Button>
+        </div>
+        <div className="recent-reviews">
+          <h3 className="recent-reviews-title">
+            5 reviews for{" "}
+            <span> Breathing Otter Baby Soothing Sound and Light Plush Doll Toy </span>
+          </h3>
+          <div className="recent-reviews-list">
+            <ReviewItems />
+            <ReviewItems />
+            <ReviewItems />
+            <ReviewItems />
+          </div>
+        </div>
+      </div>
+      <PostReview />
+    </>
+  );
+};
+
+ReviewDataItem.propTypes = {
+  rating: PropTypes.number.isRequired,
+  percentage: PropTypes.number.isRequired,
+};
 export default Review;
