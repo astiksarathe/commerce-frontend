53bade4a7d09fe60f88642f83b81b5cc92df5735
astiksarathe <astiksarathe.bce@gmail.com>
2024-07-26 21:00:40 +0530

    added description and containsElement function

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,43 +1,45 @@
-export function capitalizeFirstLetters(sentence) {
-  if (!sentence) return sentence;
-  return sentence
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
-
-export function generateRandomNumber(limit) {
-  if (limit <= 5) {
-    limit = 6;
-  }
-
-  // Generate a random number between 5 (inclusive) and limit (exclusive)
-  let randomNumber = Math.floor(Math.random() * (limit - 5)) + 6;
-  return randomNumber;
-}
-// "June 21, 2024"
-export function formatDate1(dateStr) {
-  const date = new Date(dateStr);
-
+/**
+ * Formats a date as a string with the format "Month Day, Year".
+ *
+ * @param {Date} date - The date to format.
+ * @returns {string} - The formatted date string.
+ */
+export function formatDate(date) {
   const options = { year: "numeric", month: "long", day: "numeric" };
   return date.toLocaleDateString("en-US", options);
 }
 
+/**
+ * Calculates the percentage of a value relative to a total.
+ *
+ * @param {number} total - The total value.
+ * @param {number} value - The value to calculate the percentage of.
+ * @returns {number} - The percentage value, rounded to two decimal places.
+ */
 export function calculatePercentage(total, value) {
   if (total === 0) return 0;
   return Number(((value / total) * 100).toFixed(2));
 }
 
+/**
+ * Converts an object to a query string.
+ *
+ * @param {Object} obj - The object to convert.
+ * @returns {string} - The resulting query string.
+ */
 export function getQueryParamsFromObj(obj) {
   if (!obj) return "";
-  let query = "";
-  Object.keys(obj).forEach((key) => {
-    query += `${key}=${obj[key]}&`;
-  });
-  console.log({ query });
-  return query;
+  return Object.keys(obj)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+    .join("&");
 }
 
+/**
+ * Retrieves metadata from product details.
+ *
+ * @param {Object} productDetails - The product details object.
+ * @returns {Object} - The metadata object.
+ */
 export function getMetaDataofReview(productDetails) {
   return {
     total_reviews: +productDetails.totalReviews || 0,
@@ -49,4 +51,16 @@ export function getMetaDataofReview(productDetails) {
     aggregateRating: productDetails.aggregateRating,
     Id: "asdfghjkl",
   };
+}
+
+/**
+ * Checks whether an element with a specific field value exists in the list.
+ *
+ * @param {Array<Object>} list - The list of objects to check.
+ * @param {*} value - The value to match against.
+ * @param {string} field - The field in the objects to match the value against.
+ * @returns {boolean} - True if an element with the specified field value exists, false otherwise.
+ */
+export function containsElement(list, value, field) {
+  return list.some((item) => item[field] === value);
 }
