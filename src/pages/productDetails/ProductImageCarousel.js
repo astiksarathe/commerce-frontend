d1c09d8ccaf19610728d92984e334772b34a758f
astiksarathe <astiksarathe.bce@gmail.com>
2024-08-08 23:32:03 +0530

    Improved carousel responsiveness

--- src/pages/productDetails/ProductImageCarousel.js
+++ src/pages/productDetails/ProductImageCarousel.js
@@ -15,7 +15,7 @@ const ProductImageCarousel = () => {
     verticalSwiping: true,
     responsive: [
       {
-        breakpoint: 800,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -28,33 +28,65 @@ const ProductImageCarousel = () => {
   };
   return (
     <div className="flex sticky top-32">
-      <div className="w-full p-3 pb-4 m-auto md:w-40 md:pt-0">
+      <div className="w-full max-w-lg p-0 py-2 m-auto lg:w-40 md:pt-0 md:p-3">
         <Slider {...settings}>
           <div onClick={() => setThumbnilImg("/assets/product/1.webp")}>
-            <img className="rounded m-auto" src="/assets/product/1.webp" alt="product" />
+            <img
+              className="rounded m-auto aspect-square object-cover"
+              src="/assets/product/1.webp"
+              alt="product"
+            />
           </div>
           <div onClick={() => setThumbnilImg("/assets/product/2.webp")}>
-            <img className="rounded m-auto" src="/assets/product/2.webp" alt="product" />
+            <img
+              className="rounded m-auto aspect-square object-cover"
+              src="/assets/product/2.webp"
+              alt="product"
+            />
           </div>
           <div onClick={() => setThumbnilImg("/assets/product/3.webp")}>
-            <img className="rounded m-auto" src="/assets/product/3.webp" alt="product" />
+            <img
+              className="rounded m-auto aspect-square object-cover"
+              src="/assets/product/3.webp"
+              alt="product"
+            />
           </div>
           <div onClick={() => setThumbnilImg("/assets/product/4.png")}>
-            <img className="rounded m-auto" src="/assets/product/4.png" alt="product" />
+            <img
+              className="rounded m-auto aspect-square object-cover"
+              src="/assets/product/4.png"
+              alt="product"
+            />
           </div>
           <div onClick={() => setThumbnilImg("/assets/product/6.png")}>
-            <img className="rounded m-auto" src="/assets/product/6.png" alt="product" />
+            <img
+              className="rounded m-auto aspect-square object-cover"
+              src="/assets/product/6.png"
+              alt="product"
+            />
           </div>
           <div onClick={() => setThumbnilImg("/assets/product/7.webp")}>
-            <img className="rounded m-auto" src="/assets/product/7.webp" alt="product" />
+            <img
+              className="rounded m-auto aspect-square object-cover"
+              src="/assets/product/7.webp"
+              alt="product"
+            />
           </div>
           <div onClick={() => setThumbnilImg("/assets/product/8.jpg")}>
-            <img className="rounded m-auto" src="/assets/product/8.jpg" alt="product" />
+            <img
+              className="rounded m-auto aspect-square object-cover"
+              src="/assets/product/8.jpg"
+              alt="product"
+            />
           </div>
         </Slider>
       </div>
-      <div className="custom-hidden hidden md:block md:w-full md:px-2">
-        <img className="rounded m-auto" src={thumbnilImg} alt="product thumbnil" />
+      <div className="max-w-lg custom-hidden hidden lg:block md:w-full md:px-2">
+        <img
+          className="rounded m-auto aspect-square object-cover"
+          src={thumbnilImg}
+          alt="product thumbnil"
+        />
       </div>
     </div>
   );
