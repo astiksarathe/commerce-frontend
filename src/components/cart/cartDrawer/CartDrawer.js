f8320a8542bfa67b7e5422e4e8ae53055272efcc
astiksarathe <astiksarathe.bce@gmail.com>
2024-08-10 14:01:14 +0530

    Cart Drawer from scss to tailwind converted

--- src/components/cart/cartDrawer/CartDrawer.js
+++ src/components/cart/cartDrawer/CartDrawer.js
@@ -1,17 +1,26 @@
-import { MinusOutlined } from "@ant-design/icons";
-import { Button, Drawer, Space } from "antd";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart, cartDrawerHandler, removeFromCart } from "../../../features/cart";
 import { useNavigate, Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Button, Drawer, Space } from "antd";
+import { MinusOutlined } from "@ant-design/icons";
+
+import {
+  addToCart,
+  cartDrawerHandler,
+  removeFromCart,
+} from "../../../features/cart";
+
 import QtyInput from "../../qtyInput";
-import "./cartDrawer.scss";
+
 import { formatCurrency } from "../../../utils/common";
 
 const CartDrawer = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { isCartDrawerOpen, cartList, cartValue } = useSelector((state) => state.cart);
+  const { isCartDrawerOpen, cartList, cartValue } = useSelector(
+    (state) => state.cart
+  );
   const removeCart = (product) => {
     dispatch(removeFromCart({ ...product }));
   };
@@ -39,13 +48,19 @@ const CartDrawer = () => {
         </Space>
       }
       footer={
-        <div className="cart__drawer-footer">
-          <div className="cart__drawer-subtotal">
-            <p>Subtotal</p>
-            <p>{formatCurrency(cartValue)}</p>
+        <div className="mt-4 mb-12 gap-1">
+          <div className="flex justify-between text-zinc-900 text-base leading-4">
+            <div>Subtotal</div>
+            <div>{formatCurrency(cartValue)}</div>
           </div>
-          <p className="cart__drawer-shipping">Shipping and taxes calculated at checkout.</p>
-          <Button className="btn-black" size="large" type="primary" block>
+          <div className="text-zinc-600 text-sm leading-5 mt-1 mb-4">
+            Shipping and taxes calculated at checkout.
+          </div>
+          <Button
+            className="bg-zinc-950 text-white hover:bg-zinc-900"
+            size="large"
+            block
+          >
             Check out
           </Button>
           <Button type="link" block onClick={() => navigate("/cart")}>
@@ -54,43 +69,44 @@ const CartDrawer = () => {
         </div>
       }
     >
-      <ul className="cart">
+      <ul>
         {cartList.map((product) => (
-          <li className="cart-item" key={product.productId}>
-            <div className="cart-item__image-container">
+          <li className="flex mb-4" key={product.productId}>
+            <div className="size-24 overflow-hidden flex-shrink-0 rounded">
               <img
                 src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg"
                 alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
-                className="cart-item__image"
+                className="object-cover object-center"
               />
             </div>
-            <div className="cart-item__details">
-              <div className="cart-item__title-container">
+            <div className="mt-4 flex flex-1 flex-col space-y-1">
+              <div className="flex justify-between text-base font-medium leading-6 text-black">
                 <h3>
-                  <Link to={`/${product.url}`} className="cart-item__title">
+                  <Link
+                    to={`/${product.url}`}
+                    className="no-underline hover:text-zinc-500"
+                  >
                     {product.title}
                   </Link>
                 </h3>
               </div>
-              <p className="cart-item__price">{formatCurrency(product.sellingPrice)}</p>
-              <div className="cart-item__actions">
-                <p className="cart-item__quantity">
+              <div>{formatCurrency(product.sellingPrice)}</div>
+              <div className="flex flex-1 justify-between items-end text-sm leading-5 pt-2">
+                <div className="text-gray-500">
                   <QtyInput
                     value={product.quantity}
                     quantityHandler={(quantity) => {
                       productQuantityHandler(product, quantity);
                     }}
                   />
-                </p>
-                <div>
-                  <button
-                    type="button"
-                    className="bg-transparent cart-item__remove-button"
-                    onClick={() => removeCart(product)}
-                  >
-                    Remove
-                  </button>
                 </div>
+                <button
+                  type="button"
+                  className="bg-transparent text-indigo-600 cursor-pointer font-medium hover:text-indigo-500"
+                  onClick={() => removeCart(product)}
+                >
+                  Remove
+                </button>
               </div>
             </div>
           </li>
