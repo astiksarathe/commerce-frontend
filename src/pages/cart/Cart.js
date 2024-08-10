972e7b8e391c1e74f43236c8f8da6fca97388a42
astiksarathe <astiksarathe.bce@gmail.com>
2024-08-10 22:08:52 +0530

    Cart Page scss to tailwind

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,20 +1,24 @@
 import React, { useEffect } from "react";
-import { EmptyCart } from "../../components/cart";
 import { useDispatch, useSelector } from "react-redux";
-import { cartDrawerHandler } from "../../features/cart";
+
+import { EmptyCart } from "../../components/cart";
 import { CartProductList, CartSummary } from "../../components/cart";
-import "./cart.scss";
+
+import { cartDrawerHandler } from "../../features/cart";
+
 const Cart = () => {
   const dispatch = useDispatch();
   const { cartList } = useSelector((state) => state.cart);
+
   useEffect(() => {
     dispatch(cartDrawerHandler(false));
   }, [dispatch]);
+
   return (
-    <div className="container">
+    <div className="container mx-auto p-2 sm:p-0">
       {cartList.length === 0 && <EmptyCart />}
       {cartList.length !== 0 && (
-        <div className="cart-main-container">
+        <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
           <CartProductList carts={cartList} />
           <CartSummary carts={cartList} />
         </div>
